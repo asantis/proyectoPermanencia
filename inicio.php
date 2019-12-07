eb40135d2218e6305a0687810053119b5a127cb8
Alejandro Santis <al.santisg@gmail.com>
2019-12-06 23:43:52 -0300

    Pagina Inicio - Botones
    Botones Ingreso , salida e Informes

--- inicio.php
+++ inicio.php
@@ -3,9 +3,37 @@
 <head>
 <meta charset="utf-8">
 <title>Documento sin título</title>
+<style type="text/css">
+#apDiv1 {
+	position: absolute;
+	width: 207px;
+	height: 126px;
+	z-index: 1;
+	left: 10px;
+	top: 50px;
+}
+#apDiv2 {
+	position: absolute;
+	width: 168px;
+	height: 43px;
+	z-index: 1;
+	left: 10px;
+	top: 83px;
+}
+</style>
 </head>
 
 <body>
-Prueba de conexion exitosa!! 
+<form name="form1" method="post" action="ingreso.php">
+  
+  <input type="submit" name="Registrar Ingreso" id="Registrar Ingreso" value="Registrar Ingreso">
+</form>
+<form name="form2" method="post" action="salida.php">
+  <input type="submit" name="Registrar Salida" id="Registrar Salida" value="Registrar Salida">
+</form>
+<form name="form3" method="post" action="informes.php">
+  <input type="submit" name="Informes" id="Informes" value="Informes">
+</form>
+<p>&nbsp;</p>
 </body>
 </html>
